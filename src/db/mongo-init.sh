178f7298ac3e8e93fa8c8bdd89b23d6099e79b5e
Esteban Alvarez <aeap19980929@gmail.com>
2020-11-30 08:11:37 -0600

    Agregando comando en mongo init

--- src/db/mongo-init.sh
+++ src/db/mongo-init.sh
@@ -1,3 +1,7 @@
-mongo --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); \
-db = db.getSiblingDB('$MONGO_DB'); db.createUser({ user: '$MONGO_USERNAME', \
+mongo --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD \
+ --eval "db = db.getSiblingDB('$MONGO_DB'); \
+db.createUser({ user: '$MONGO_USERNAME', \
 pwd: '$MONGO_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGO_DB' }] });"
+
+mongo $MONGO_DB --username $MONGO_USERNAME --password $MONGO_PASSWORD \
+--eval "db.createCollection('$MONGO_COLLECTION')"
